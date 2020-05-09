ccd9e9391dd8f29105a3c6f65c96fa8b4cd21331
Nixon Augustine <nixon_augustine@hotmail.com>
2020-05-09 23:05:14 +0530

    Added Function to Display Change of Boolean Value

--- app/app.component.ts
+++ app/app.component.ts
@@ -25,6 +25,13 @@ export class AppComponent {
   ageGroup : number[] = [1,2,3,4,5];
   namesList : string[] = ['Edward', 'Nixon', 'Augustine'];
 
+ delay(t){
+    setInterval(()=>{
+      this.showDiv= !this.showDiv;
+      console.log(this.showDiv);
+    },t*1000);
+  }
 
+  
 
 }
